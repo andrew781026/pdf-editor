aa46efab95eb731db6b7c847a7a4e47a034f5bfd
andrew_wang <andrew_wang@friday.tw>
2022-02-24 16:23:03 +0800

    add fabric.js

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Board from './components/Board.vue'
+import Fabric from './components/Fabric.vue'
 </script>
 
 <template>
-  <Board/>
+  <div class="flex flex-col">
+    <Board/>
+    <Fabric/>
+  </div>
 </template>
 
 <style>
@@ -23,7 +27,15 @@ body {
   font-family: '微軟正黑體', '蘋果儷黑體', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center; 這行會造成 cytoscape 內的 canvas 位置顯示異常 */
+  /* text-align: center; 這行會造成 cytoscape 內的 canvas 位置顯示異常 */
   color: #2c3e50;
+}
+
+.flex {
+  display: flex;
+}
+
+.flex-col {
+  flex-direction: column;
 }
 </style>
---
+++ src/components/Fabric.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="root">
+    <div class="btn-wrap">
+      <button @click="add">新增圈圈</button>
+      <button @click="add">新增圈圈</button>
+      <button @click="add">新增圈圈</button>
+      <button @click="add">新增圈圈</button>
+    </div>
+    <canvas class="fabric" ref="canvas" height="400" width="1000"></canvas>
+  </div>
+</template>
+
+<script>
+import {fabric} from 'fabric';
+
+export default {
+  name: "Fabric",
+  mounted() {
+
+    this.canvas = new fabric.Canvas(this.$refs.canvas);
+  },
+  data() {
+
+    return {
+      canvas: null,
+      counter: 1,
+    }
+  },
+  methods: {
+    add() {
+
+      const rect = new fabric.Rect({
+        top: 100,
+        left: 100,
+        width: 60,
+        height: 70,
+        fill: 'red'
+      });
+
+      this.canvas.add(rect);
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  font-size: 1rem;
+}
+
+.root {
+  display: flex;
+  background-color: rgb(89, 162, 230);
+  margin-top: 20px;
+}
+
+.btn-wrap {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.fabric {
+  border: 3px solid #333;
+  width: 500px;
+  height: 500px;
+  background-color: #fff;
+}
+</style>
